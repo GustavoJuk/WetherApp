43d66b418bcd818834eb56669ff1ef1cc66f4763
gjukferreiracruz <gjukferreiracruz@apple.com>
2025-01-24 12:38:35 -0300

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gustavo Juk on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
